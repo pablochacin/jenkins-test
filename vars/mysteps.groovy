09677cf601cc8d00739803997fd9cd75b687e3c9
pablochacin <pchacin@suse.com>
2020-07-16 09:45:40 +0200

    Update mysteps.groovy

--- vars/mysteps.groovy
+++ vars/mysteps.groovy
@@ -1,7 +1,11 @@
 def build(String target) {
-    echo "building ${target}..."
+    stage{
+       echo "building ${target}..."
+    }
 }
 
 def test(String target) {
-    echo "testing ${target}..."
+    stage {
+       echo "testing ${target}..."
+    }
 }
